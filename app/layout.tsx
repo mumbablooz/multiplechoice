19cc607ff4429c3012cb8f08e1d575dcd5daaeb6
Blooz <89782115+mumbablooz@users.noreply.github.com>
2024-01-02 11:29:24 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Link from 'next/link'
-
+import Image from 'next/image'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -15,10 +15,26 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  
   return (
     <html lang="de">
       <body className={inter.className}>
-      <Link href={'/'}><h2>Multiple Choice zum lernen</h2></Link>
+        <div style={{
+          display: 'flex',
+          alignItems: 'center',
+          padding: '0.3rem'
+        }}>
+        <Link href={'/'}><h2 style={{
+          margin: '0 1rem'
+        }}>Multiple Choice zum lernen</h2></Link>
+      <Image 
+          src={'https://cdn.pixabay.com/photo/2014/09/12/18/23/selection-443127_1280.png'} 
+          width={414} 
+          height={608} 
+          style={{width: '3rem', height: 'auto'}}
+          alt='Bundesland Bild' />
+        </div>
+      
         {children}
         </body>
     </html>
